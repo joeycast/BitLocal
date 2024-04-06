9bfd1a41d59276f3f585533f23c59724d2897c57
Joey Castagnaro <joey.castagnaro@me.com>
2024-04-06 12:41:06 -0500

    v1.4 (21) - Additional API call enhancements
    - Updated API call to also run after app becomes active again

--- App/MyApp.swift
+++ App/MyApp.swift
@@ -4,6 +4,12 @@ import SwiftUI
 @available(iOS 16.4, *)
 struct MyApp: App {
     
+    // Add the environment property to track the app's scene phase
+    @Environment(\.scenePhase) var scenePhase
+    
+    // Initialize your API manager
+    let apiManager = APIManager()
+    
     init() {
         MyFont.registerFonts()
     }
@@ -11,6 +17,16 @@ struct MyApp: App {
     var body: some Scene {
         WindowGroup {
             MainView()
+            // Use .onChange(of:) to react to changes in the scenePhase
+                .onChange(of: scenePhase) { newPhase in
+                    if newPhase == .active {
+                        // The app has become active; trigger API call
+                        apiManager.getElements { _ in
+                            // Handle the fetched data
+                            // Note: Ensure any UI updates are dispatched on the main thread
+                        }
+                    }
+                }
         }
     }
 }
--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
             bundleIdentifier: "app.bitlocal.bitlocal",
             teamIdentifier: "5YUMLYCFT8",
             displayVersion: "1.4",
-            bundleVersion: "20",
+            bundleVersion: "21",
             appIcon: .asset("AppIcon"),
             accentColor: .presetColor(.orange),
             supportedDeviceFamilies: [
