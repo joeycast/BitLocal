1fc04bb4802112130c591e3172f8949c06482eee
joeycast <joey.castagnaro@me.com>
2025-04-06 16:00:49 -0500

    Initial Commit

---
+++ bitlocal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bitlocal
+//
+//  Created by Joe Castagnaro on 4/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bitlocal/bitlocalApp.swift
@@ -0,0 +1,17 @@
+//
+//  bitlocalApp.swift
+//  bitlocal
+//
+//  Created by Joe Castagnaro on 4/6/25.
+//
+
+import SwiftUI
+
+@main
+struct bitlocalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
